939c3f6ed5a35d2c2d2a23abf4f419d2e53de8d0
LeiderJeil <l.ticlla@clasesumss.com>
2017-02-23 21:38:43 -0400

    primer commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include('views/header.php'); ?>
+    <div id="main">
+		<div class="ic"></div>
+    	<?php include('views/sidebar.php'); ?>
+    	<div id="right-part">
+        	<h1>Headline</h1>
+            <p><img src="img/image.jpg" alt="" width="251" height="196" class="right" />Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Vestibulum ornare elementum neque at lobortis. 
+            Fusce ac orci diam. Ut ac tellus in lorem commodo vulputate a vitae ligula. Proin nisl lectus, lacinia id 
+            egestas eu, hendrerit id purus. In accumsan felis in sem pulvinar condimentum eget sed odio. Etiam sapien 
+            leo, tincidunt at vulputate quis, suscipit eget lacus. Nunc vestibulum felis ut eros adipiscing at ornare 
+            magna vulputate. Nulla viverra malesuada malesuada. Vestibulum ante ipsum primis in faucibus orci luctus 
+            et ultrices posuere cubilia Curae;</p>
+
+            <p>Mauris id est mi, varius <a href="#">dignissim lectus</a>. Sed ante lorem, vulputate sed tempor tristique, elementum quis 
+            tortor. Etiam consequat, sapien id eleifend suscipit, odio felis consequat enim, quis tincidunt leo magna 
+            semper ante. Phasellus ut felis eget felis adipiscing aliquam a vitae dolor. Pellentesque pulvinar condimentum 
+            nibh, eu vulputate justo fermentum et. Ut sit amet augue fermentum elit dignissim accumsan. Vestibulum ante 
+            ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi et quam vitae dolor ornare 
+            laoreet. Ut feugiat consequat tellus id tincidunt. Duis a placerat eros. Proin vel arcu at ipsum hendrerit 
+            aliquam. Nullam quam ipsum, sagittis non vehicula non, pellentesque ut dolor.</p>
+            
+            <h1>H1 Headline</h1>
+
+            <h2>H2 Headline</h2>
+            
+            <p>List:</p>
+			<ul class="list">
+                <li>Nulla viverra malesuada malesuada. Vestibulum ante ipsum primis in faucibus</li>
+                <li>Nulla viverra malesuada malesuada. Vestibulum ante ipsum primis in faucibus</li>
+                <li>Nulla viverra malesuada malesuada. Vestibulum ante ipsum primis in faucibus</li>
+                <li>Nulla viverra malesuada malesuada. Vestibulum ante ipsum primis in faucibus</li>
+                <li>Nulla viverra malesuada malesuada. Vestibulum ante ipsum primis in faucibus</li>
+            </ul>
+
+            <p><a href="#">This</a> is a link. </p>
+        </div>
+    </div>
+<?php include('views/footer.php'); ?>
---
+++ views/sidebar.php
@@ -0,0 +1,16 @@
+<div id="left-part">
+        	<h1>Category</h1>
+            <ul id="subnavi">
+                <li class="active"><a href="#">Submenu item</a></li>
+                <li><a href="#">Submenu item</a></li>
+                <li><a href="#">Submenu item</a></li>
+                <li><a href="#">Submenu item</a></li>
+                <li><a href="#">Submenu item</a></li>
+                <li><a href="#">Submenu item</a></li>
+            </ul>
+            
+            <h1>Additional content</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare elementum neque at lobortis. 
+            Fusce ac orci diam. Ut ac tellus in lorem commodo vulputate a vitae ligula. Proin nisl lectus, lacinia id 
+            egestas eu, hendrerit id purus.</p>
+        </div>
